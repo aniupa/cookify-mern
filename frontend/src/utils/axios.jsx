82e5e385c5cc01c2623c443097470a4b3cf2760f
Anirudh <140832676+aniupa@users.noreply.github.com>
2026-01-11 21:12:36 +0530

    minor changes required for deployment

--- frontend/src/utils/axios.jsx
+++ frontend/src/utils/axios.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios'
+import dotenv from "dotenv";
+dotenv.config();
+
 export const instance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: ['http://localhost:3000/api' || `${process.env.REACT_APP_API_URL}/api`],
 //   timeout: 1000,
   headers: {'X-Custom-Header': 'foobar'}
 });
